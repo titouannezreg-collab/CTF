e2b0408d43fe252c26937543d6fc1cceba6a1e5c
titouannezreg-collab <titouannezreg@gmail.com>
2026-02-28 22:30:01 +0100

    Delete ctf directory

--- ctf/ne pas toucher/web/defi3/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("btn").addEventListener("click", () => {
-    alert("Votre avenir est déjà écrit ⭐");
-});
-
-// suite du flag (discrète)
-const a = ["pas_", "ce_que_", "tu_vois", "}"];
-window._end = a.join("");
